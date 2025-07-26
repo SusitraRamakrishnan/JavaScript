5b942e170098bb9965df19ebe7a530e48689911a
Susitra_Ramakrishnan <susitraramakrishnan@gmail.com>
2025-07-26 19:29:02 +0530

    Commit -2

--- FirstFile.js
+++ FirstFile.js
@@ -4,3 +4,12 @@ var marks=[30,40,50,60,70]
  console.log(len)
  marks.push(60)
  console.log(marks.length)
+
+ //reduce function is used when we  need to perform opertation on all the elements of the array
+ 
+ let sum=marks.reduce((sum,totalMarks)=>sum+totalMarks,0)
+  console.log(sum)
+ //filter 
+ let modified=marks.filter(score=>score%2==0) 
+ console.log(modified)
+
